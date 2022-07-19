77d81e38453537e7763f6177661cd2c4c820fc74
Daniel Davidson <DanDavidson88@gmail.com>
2022-07-18 23:58:23 -0700

    updates

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,7 +5,7 @@ function Result({ result, openDetail }) {
     return (
       <div className="result" onClick=
           {() => openDetail(result.imdbID)}>
-        <img src={result.Poster} alt="poster" />
+        <img src={result.Poster} alt="" />
         {/* <h3>{result.Title}</h3> */}
       </div>
     );
